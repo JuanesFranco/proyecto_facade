48d940d537808d4fd2002276ddb823fa4f580285
Juan Esteban Franco <jefranco93189@umanizales.edu.co>
2023-03-30 16:14:14 -0500

    Initial commit

---
+++ src/mitocode/App.java
@@ -0,0 +1,16 @@
+package mitocode;
+
+import mitocode.facade.CheckFacade;
+
+public class App {
+	
+	public static void main(String[] args) {
+		CheckFacade cliente1 = new CheckFacade();
+		cliente1.buscar("02/07/2018", "08/07/2018", "Lima", "Canc�n");
+		
+		CheckFacade cliente2 = new CheckFacade();
+		cliente2.buscar("02/07/2018", "08/07/2018", "Lima", "Quito");
+	}
+}
+
+
